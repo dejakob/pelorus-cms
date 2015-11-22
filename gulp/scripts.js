250f45483733c7856ebc6c01e69ce61ec6327bd2
Jakezville <jakob.kerkhove@showpad.com>
2015-11-22 01:02:30 +0100

    automatic version numbers

--- gulp/scripts.js
+++ gulp/scripts.js
@@ -2,6 +2,7 @@
 
 const LOAD_PLUGINS_CONFIG = { camelize: true };
 
+let config  = require('../package.json');
 let gulp    = require('gulp');
 let plugins = require('gulp-load-plugins')(LOAD_PLUGINS_CONFIG);
 let wiredep = require('wiredep').stream;
@@ -11,9 +12,19 @@ gulp.task('scripts', scriptsTask);
 
 function scriptsTask ()
 {
+    const VERSION = config.version || '1.0.0';
+    const VERSION_CONFIG = {
+        value: VERSION,
+        replaces: [
+            '{APP_VERSION}',
+            [/\{APP_VERSION\}/g, VERSION]
+        ]
+    };
+
     return gulp
         .src(paths.frontend.js)
         .pipe(plugins.concat('script.js'))
+        .pipe(plugins.versionNumber(VERSION_CONFIG))
         .pipe(plugins.uglify())
         .pipe(gulp.dest(paths.frontend.dist.js));
 }
